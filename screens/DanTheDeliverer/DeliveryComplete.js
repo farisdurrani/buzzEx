118d09b62ce123ff821fa2ee73c8817289b4a87a
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-04-02 12:06:44 -0400

    adding delivery complete screen for dan

--- screens/DanTheDeliverer/DeliveryComplete.js
+++ screens/DanTheDeliverer/DeliveryComplete.js
@@ -1,52 +1,117 @@
+import React, { useEffect, useState } from "react";
 import {
-    StyleSheet,
-    Text,
     TouchableOpacity,
-    View,
-    KeyboardAvoidingView,
-    Image,
+    StyleSheet,
     TextInput,
-  } from "react-native";
-  import React, { useState } from "react";
-  import { BButton, BackCancelButtons } from "../../components/index";
-  import { AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons";
-  import { COLORS, LAYOUT } from "../../constants";
-  
-  const DeliveryComplete = ({ navigation, route }) => {
+    View,
+    Image,
+    Dimensions
+} from "react-native";
+import { Input, Button, Text, useTheme } from 'react-native-elements';
+import { BButton } from "../../components/index";
+import { AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Icon from 'react-native-vector-icons/Feather';
+import MapComponent from "../../components/MapComponent";
+
+let {width, height} = Dimensions.get('window') //Screen dimensions
+const ASPECT_RATIO = width/height
+const LATITUDE_DELTA = 0.04  // Controls the zoom level of the map. Smaller means more zoomed in
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO // Dependent on LATITUDE_DELTA
+
+const DeliveryComplete = ({ route, navigation }) => {
+    const [deliveryNotes, onAddDeliveryNotes] = useState("");
+    const { mapProps } = route.params;
+    const hasLocationData = mapProps.latitude !== null && mapProps.longitude !== null;
     return (
-      <View style={styles.mainContainer}>
-        <BackCancelButtons navigation={navigation} />
-  
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("SellerAccepted");
-          }}
-        >
-          <Text style={styles.awaitingText}>Awaiting for Bob to Accept...</Text>
-        </TouchableOpacity>
-      </View>
+            <View style={styles.container}>
+                
+                <View style={styles.topleftbutton}>
+                
+                <TouchableOpacity onPress={navigation.goBack}>
+                  <Ionicons name="arrow-back" size={24} color="black" />
+                </TouchableOpacity>
+                
+              </View>
+            {hasLocationData ? 
+                <MapComponent mapProps={{...mapProps, style: styles.map}}/>
+                : <Text style={styles.paragraph}>Loading Map...</Text>}
+            <View style={styles.bottomContainer}>
+                <Text style={styles.linetwo}>Delivery Complete!</Text>
+            </View>
+          </View>
     );
-  };
-  
-  const styles = StyleSheet.create({
-    mainContainer: {
-      height: "95%",
-      paddingVertical: 60,
-      paddingHorizontal: 20,
-      alignItems: "center",
+};
+
+export default DeliveryComplete;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
-    upperButtons: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      width: "100%",
+    lineone: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+      linetwo: {
+        paddingBottom: 25,
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+      linethree: {
+        paddingBottom: 25,
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+    button: {
+        padding: 20
     },
-    awaitingText: {
-      marginTop: 300,
-      fontSize: 18,
-      fontWeight: "bold",
+    buttonView: {
+        position: "absolute",
+        top: 5/6 * height
     },
-  });
-  
-  export default DeliveryComplete;
-  
+    buttonContainer: {},
+    buttonOutline: {},
+    buttonOutlineText: {},
+    buttonText: {
+        color: "blue",
+    },
+    input: {
+        padding: 15,
+        borderBottomWidth: 1,
+        borderColor: "rgba(0, 0, 0, .2)"
+    },
+    inputContainer: {},
+    headingContainer: {
+        position: "absolute",
+        top:50
+    },
+    bottomContainer: {
+        position: "absolute",
+        bottom:25
+    },
+    font: {
+      textAlign: "center",
+      maxWidth: 200,
+      fontWeight: 'bold'
+    },
+    topleftbutton: {
+        position: "absolute",
+        left: 25,
+        top: 50,
+    },
+    toprightbutton: {
+        position: "absolute",
+        right: 25,
+        top: 50,
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        top: 1/7 * height,
+        bottom: 1/3 * height
+      }
+});
