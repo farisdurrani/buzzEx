74d380b27840b7154356ddb4f31e3522ddf4a0cf
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-04-22 22:49:47 -0400

    adding buttons and instruction text

--- screens/SamTheSeller/sizeSelection.js
+++ screens/SamTheSeller/sizeSelection.js
@@ -2,31 +2,54 @@ import {StyleSheet, Text, View} from 'react-native'
 import { BackCancelButtons } from '../../components';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import React from 'react'
+import { BButton } from "../../components/index";
 
 const SizeSelection = ({ navigation, route }) => {
   return (
     <View style= {styles.Maincontainer}>
         <BackCancelButtons style = {styles.backcancel} navigation={navigation} />
+        <View  style={styles.containerText}>
+        <Text  style={styles.selectionText}>Select Vehicle Type</Text>
+        </View>
         <View style = {styles.container}>
             
             <View style = {styles.box}>
                 <View style = {styles.inner}>
                 <Icon size={80} name="bicycle"></Icon>
+                <BButton
+                    text="Confirm"
+                    onPress={console.log("bike")}
+                />
                 </View>
             </View>
 
             <View style = {styles.box}>
                 <View style = {styles.inner}>
+                <Icon size={80} name="car-side"></Icon>
+                <BButton
+                    text="Confirm"
+                    onPress={console.log("car")}
+                />
                 </View>
             </View>
 
             <View style = {styles.box}>
                 <View style = {styles.inner}>
+                <Icon size={80} name="truck-pickup"></Icon>
+                <BButton
+                    text="Confirm"
+                    onPress={console.log("truck")}
+                />
                 </View>
             </View>
 
             <View style = {styles.box}>
                 <View style = {styles.inner}>
+                <Icon size={80} name="trailer"></Icon>
+                <BButton
+                    text="Confirm"
+                    onPress={console.log("trailer")}
+                />
                 </View>
             </View>
 
@@ -55,9 +78,7 @@ const styles = StyleSheet.create({
     box: {
         width: "50%",
         height: "50%",
-        padding:5,
-        borderRadius: 30,
-        
+        padding:5,       
     },
 
     inner: {
@@ -66,7 +87,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    backcancel: {
+    selectionText: {
+        alignItems: 'center', 
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    containerText: {
+        alignItems: 'center', 
     }
 })
+
 
